171f8f909c3bfed4128aef2ef404a4d77495468f
Wenping Guo <ybyygu@gmail.com>
2022-04-18 16:49:38 +0800

    Remove various debugging flags for potentials

--- src/edip.rs
+++ src/edip.rs
@@ -110,14 +110,6 @@ impl EdipParams {
 // 8a0e64b1 ends here
 
 // [[file:../edip.note::d9088664][d9088664]]
-/* DEBUGGING FLAGS - systematically turn on various pieces of the potential. */
-const V2_on: bool = true;
-const V2Z_on: bool = true;
-const V3_on: bool = true;
-const V3g_on: bool = true;
-const V3h_on: bool = true;
-const V3Z_on: bool = true;
-
 #[derive(Default, Clone)]
 struct Store2 {
     // various V2 functions and derivatives
@@ -203,16 +195,15 @@ fn compute_forces_edip(
     // e_potential = e_v2 + e_v3
     let mut e_v2 = 0.0;
     let mut e_v3 = 0.0;
-    let mut coord_total = 0.0;
     let mut virial = 0.0;
 
     /* COMBINE COEFFICIENTS */
     let asqr = params.a.powi(2);
-    let Qort = params.Qo.sqrt();
-    let muhalf = params.mu * 0.5;
+    let qo_sqrt = params.Qo.sqrt();
+    let mu_half = params.mu * 0.5;
     let u5 = u2 * u4;
     let bmc = params.b - params.c;
-    let cmbinv = 1.0 / (params.c - params.b);
+    let cmb_inv = 1.0 / (params.c - params.b);
 
     /*--- LEVEL 1: OUTER LOOP OVER ATOMS ---*/
     for i in 0..n_own {
@@ -224,13 +215,11 @@ fn compute_forces_edip(
 
         /*--- LEVEL 2: LOOP PREPASS OVER PAIRS ---*/
         for &j in &neighbors[i] {
+            assert_ne!(i, j);
             /* TEST IF WITHIN OUTER CUTOFF */
             let dx = positions[j][0] - positions[i][0];
             let dy = positions[j][1] - positions[i][1];
             let dz = positions[j][2] - positions[i][2];
-            // dx = MIN_IMAGE_DISTANCE(dx,box.L_x,box.L_x_div2);
-            // dy = MIN_IMAGE_DISTANCE(dy,box.L_y,box.L_y_div2);
-            // dz = MIN_IMAGE_DISTANCE(dz,box.L_z,box.L_z_div2);
             let rsqr = dx * dx + dy * dy + dz * dz;
             if dx.abs() < params.a && dy.abs() < params.a && dz.abs() < params.a && rsqr < asqr {
                 let r = rsqr.sqrt();
@@ -257,13 +246,8 @@ fn compute_forces_edip(
                     let rmbinv = 1.0 / (r - params.bg);
                     let temp1 = params.gam * rmbinv;
                     let temp0 = temp1.exp();
-                    if V3g_on {
-                        s3[n3].g = temp0;
-                        s3[n3].dg = -rmbinv * temp1 * temp0;
-                    } else {
-                        s3[n3].g = 1.0;
-                        s3[n3].dg = 0.0;
-                    }
+                    s3[n3].g = temp0;
+                    s3[n3].dg = -rmbinv * temp1 * temp0;
                     s3[n3].dx = dxrinv;
                     s3[n3].dy = dyrinv;
                     s3[n3].dz = dzrinv;
@@ -283,7 +267,7 @@ fn compute_forces_edip(
                             let fZ = temp1.exp();
                             Z += fZ;
                             numz[nz] = j;
-                            sz[nz].df = fZ * temp1 * den * 3.0 * xinv3 * xinv * cmbinv; /* df/dr */
+                            sz[nz].df = fZ * temp1 * den * 3.0 * xinv3 * xinv * cmb_inv; /* df/dr */
                             sz[nz].dx = dxrinv;
                             sz[nz].dy = dyrinv;
                             sz[nz].dz = dzrinv;
@@ -294,7 +278,6 @@ fn compute_forces_edip(
                 }
             }
         }
-        coord_total += Z;
 
         /* ZERO ACCUMULATION ARRAY FOR ENVIRONMENT FORCES */
         for nl in 0..nz {
@@ -302,154 +285,123 @@ fn compute_forces_edip(
         }
 
         /* ENVIRONMENT-DEPENDENCE OF PAIR INTERACTION */
-        let mut pz = 0.0;
-        let mut dp = 0.0;
-        if V2_on {
-            if V2Z_on {
-                let temp0 = params.bet * Z;
-                pz = params.palp * (-temp0 * Z).exp(); /* bond order */
-                dp = -2.0 * temp0 * pz; /* derivative of bond order */
-            } else {
-                pz = params.palp * (-params.bet * 16.0).exp();
-            }
-            /*--- LEVEL 2: LOOP FOR PAIR INTERACTIONS ---*/
-            for nj in 0..n2 {
-                let temp0 = s2[nj].t1 - pz;
-                /* two-body energy V2(rij,Z) */
-                e_v2 += temp0 * s2[nj].t0;
+        let temp0 = params.bet * Z;
+        let pz = params.palp * (-temp0 * Z).exp(); /* bond order */
+        let dp = -2.0 * temp0 * pz; /* derivative of bond order */
+        /*--- LEVEL 2: LOOP FOR PAIR INTERACTIONS ---*/
+        for nj in 0..n2 {
+            let temp0 = s2[nj].t1 - pz;
+            /* two-body energy V2(rij,Z) */
+            e_v2 += temp0 * s2[nj].t0;
 
-                /* two-body forces */
-                let dV2j = -(s2[nj].t0) * ((s2[nj].t1) * (s2[nj].t2) + temp0 * (s2[nj].t3)); /* dV2/dr */
-                let dV2ijx = dV2j * s2[nj].dx;
-                let dV2ijy = dV2j * s2[nj].dy;
-                let dV2ijz = dV2j * s2[nj].dz;
-                forces[i][0] += dV2ijx;
-                forces[i][1] += dV2ijy;
-                forces[i][2] += dV2ijz;
-                let j = num2[nj];
-                forces[j][0] -= dV2ijx;
-                forces[j][1] -= dV2ijy;
-                forces[j][2] -= dV2ijz;
+            /* two-body forces */
+            let dV2j = -(s2[nj].t0) * ((s2[nj].t1) * (s2[nj].t2) + temp0 * (s2[nj].t3)); /* dV2/dr */
+            let dV2ijx = dV2j * s2[nj].dx;
+            let dV2ijy = dV2j * s2[nj].dy;
+            let dV2ijz = dV2j * s2[nj].dz;
+            forces[i][0] += dV2ijx;
+            forces[i][1] += dV2ijy;
+            forces[i][2] += dV2ijz;
+            let j = num2[nj];
+            forces[j][0] -= dV2ijx;
+            forces[j][1] -= dV2ijy;
+            forces[j][2] -= dV2ijz;
 
-                /* dV2/dr contribution to virial */
-                virial -= s2[nj].r * (dV2ijx * s2[nj].dx + dV2ijy * s2[nj].dy + dV2ijz * s2[nj].dz);
+            /* dV2/dr contribution to virial */
+            virial -= s2[nj].r * (dV2ijx * s2[nj].dx + dV2ijy * s2[nj].dy + dV2ijz * s2[nj].dz);
 
-                /*--- LEVEL 3: LOOP FOR PAIR COORDINATION FORCES ---*/
-                let dV2dZ = -dp * s2[nj].t0;
-                for nl in 0..nz {
-                    sz[nl].sum += dV2dZ;
-                }
+            /*--- LEVEL 3: LOOP FOR PAIR COORDINATION FORCES ---*/
+            let dV2dZ = -dp * s2[nj].t0;
+            for nl in 0..nz {
+                sz[nl].sum += dV2dZ;
             }
         }
 
         /* COORDINATION-DEPENDENCE OF THREE-BODY INTERACTION */
-        let mut tau = 0.0;
-        let mut dtau = 0.0;
-        let mut winv = 0.0;
-        let mut dwinv = 0.0;
-        if V3_on {
-            if V3Z_on {
-                winv = Qort * (-muhalf * Z).exp(); /* inverse width of angular function */
-                dwinv = -muhalf * winv; /* its derivative */
-                let temp0 = (-u4 * Z).exp();
-                tau = u1 + u2 * temp0 * (u3 - temp0); /* -cosine of angular minimum */
-                dtau = u5 * temp0 * (2.0 * temp0 - u3); /* its derivative */
-            } else {
-                winv = Qort * (-muhalf * 4.0).exp();
-                dwinv = 0.0;
-                tau = 1.0 / 3.0;
-                dtau = 0.0;
-            }
+        let w_inv = qo_sqrt * (-mu_half * Z).exp(); /* inverse width of angular function */
+        let dw_inv = -mu_half * w_inv; /* its derivative */
+        let temp0 = (-u4 * Z).exp();
+        let tau = u1 + u2 * temp0 * (u3 - temp0); /* -cosine of angular minimum */
+        let dtau = u5 * temp0 * (2.0 * temp0 - u3); /* its derivative */
 
-            /*--- LEVEL 2: FIRST LOOP FOR THREE-BODY INTERACTIONS ---*/
-            for nj in 0..(n3 - 1) {
-                let j = num3[nj];
-                /*--- LEVEL 3: SECOND LOOP FOR THREE-BODY INTERACTIONS ---*/
-                for nk in (nj + 1)..n3 {
-                    let k = num3[nk];
-                    /* angular function h(l,Z) */
-                    let lcos = s3[nj].dx * s3[nk].dx + s3[nj].dy * s3[nk].dy + s3[nj].dz * s3[nk].dz;
-                    let x = (lcos + tau) * winv;
-                    let temp0 = (-x * x).exp();
+        /*--- LEVEL 2: FIRST LOOP FOR THREE-BODY INTERACTIONS ---*/
+        for nj in 0..(n3 - 1) {
+            let j = num3[nj];
+            /*--- LEVEL 3: SECOND LOOP FOR THREE-BODY INTERACTIONS ---*/
+            for nk in (nj + 1)..n3 {
+                let k = num3[nk];
+                /* angular function h(l,Z) */
+                let lcos = s3[nj].dx * s3[nk].dx + s3[nj].dy * s3[nk].dy + s3[nj].dz * s3[nk].dz;
+                let x = (lcos + tau) * w_inv;
+                let temp0 = (-x * x).exp();
 
-                    let H;
-                    let dhdl;
-                    let mut dHdx = 0.0;
-                    if V3h_on {
-                        H = params.lam * (1.0 - temp0 + params.eta * x * x);
-                        dHdx = 2.0 * params.lam * x * (temp0 + params.eta);
-                        dhdl = dHdx * winv;
-                    } else {
-                        H = 1.0;
-                        dhdl = 0.0;
-                    }
+                let H = params.lam * (1.0 - temp0 + params.eta * x * x);
+                let dHdx = 2.0 * params.lam * x * (temp0 + params.eta);
+                let dhdl = dHdx * w_inv;
 
-                    /* three-body energy */
-                    let temp1 = s3[nj].g * s3[nk].g;
-                    e_v3 += temp1 * H;
+                /* three-body energy */
+                let temp1 = s3[nj].g * s3[nk].g;
+                e_v3 += temp1 * H;
 
-                    /* (-) radial force on atom j */
-                    let dV3rij = s3[nj].dg * s3[nk].g * H;
-                    let dV3rijx = dV3rij * s3[nj].dx;
-                    let dV3rijy = dV3rij * s3[nj].dy;
-                    let dV3rijz = dV3rij * s3[nj].dz;
-                    let mut fjx = dV3rijx;
-                    let mut fjy = dV3rijy;
-                    let mut fjz = dV3rijz;
+                /* (-) radial force on atom j */
+                let dV3rij = s3[nj].dg * s3[nk].g * H;
+                let dV3rijx = dV3rij * s3[nj].dx;
+                let dV3rijy = dV3rij * s3[nj].dy;
+                let dV3rijz = dV3rij * s3[nj].dz;
+                let mut fjx = dV3rijx;
+                let mut fjy = dV3rijy;
+                let mut fjz = dV3rijz;
 
-                    /* (-) radial force on atom k */
-                    let dV3rik = s3[nj].g * s3[nk].dg * H;
-                    let dV3rikx = dV3rik * s3[nk].dx;
-                    let dV3riky = dV3rik * s3[nk].dy;
-                    let dV3rikz = dV3rik * s3[nk].dz;
-                    let mut fkx = dV3rikx;
-                    let mut fky = dV3riky;
-                    let mut fkz = dV3rikz;
+                /* (-) radial force on atom k */
+                let dV3rik = s3[nj].g * s3[nk].dg * H;
+                let dV3rikx = dV3rik * s3[nk].dx;
+                let dV3riky = dV3rik * s3[nk].dy;
+                let dV3rikz = dV3rik * s3[nk].dz;
+                let mut fkx = dV3rikx;
+                let mut fky = dV3riky;
+                let mut fkz = dV3rikz;
 
-                    /* (-) angular force on j */
-                    let dV3l = temp1 * dhdl;
-                    let dV3ljx = dV3l * (s3[nk].dx - lcos * s3[nj].dx) * s3[nj].rinv;
-                    let dV3ljy = dV3l * (s3[nk].dy - lcos * s3[nj].dy) * s3[nj].rinv;
-                    let dV3ljz = dV3l * (s3[nk].dz - lcos * s3[nj].dz) * s3[nj].rinv;
-                    fjx += dV3ljx;
-                    fjy += dV3ljy;
-                    fjz += dV3ljz;
+                /* (-) angular force on j */
+                let dV3l = temp1 * dhdl;
+                let dV3ljx = dV3l * (s3[nk].dx - lcos * s3[nj].dx) * s3[nj].rinv;
+                let dV3ljy = dV3l * (s3[nk].dy - lcos * s3[nj].dy) * s3[nj].rinv;
+                let dV3ljz = dV3l * (s3[nk].dz - lcos * s3[nj].dz) * s3[nj].rinv;
+                fjx += dV3ljx;
+                fjy += dV3ljy;
+                fjz += dV3ljz;
 
-                    /* (-) angular force on k */
-                    let dV3lkx = dV3l * (s3[nj].dx - lcos * s3[nk].dx) * s3[nk].rinv;
-                    let dV3lky = dV3l * (s3[nj].dy - lcos * s3[nk].dy) * s3[nk].rinv;
-                    let dV3lkz = dV3l * (s3[nj].dz - lcos * s3[nk].dz) * s3[nk].rinv;
-                    fkx += dV3lkx;
-                    fky += dV3lky;
-                    fkz += dV3lkz;
+                /* (-) angular force on k */
+                let dV3lkx = dV3l * (s3[nj].dx - lcos * s3[nk].dx) * s3[nk].rinv;
+                let dV3lky = dV3l * (s3[nj].dy - lcos * s3[nk].dy) * s3[nk].rinv;
+                let dV3lkz = dV3l * (s3[nj].dz - lcos * s3[nk].dz) * s3[nk].rinv;
+                fkx += dV3lkx;
+                fky += dV3lky;
+                fkz += dV3lkz;
 
-                    /* apply radial + angular forces to i, j, k */
-                    forces[j][0] -= fjx;
-                    forces[j][1] -= fjy;
-                    forces[j][2] -= fjz;
-                    forces[k][0] -= fkx;
-                    forces[k][1] -= fky;
-                    forces[k][2] -= fkz;
-                    forces[i][0] += fjx + fkx;
-                    forces[i][1] += fjy + fky;
-                    forces[i][2] += fjz + fkz;
+                /* apply radial + angular forces to i, j, k */
+                forces[j][0] -= fjx;
+                forces[j][1] -= fjy;
+                forces[j][2] -= fjz;
+                forces[k][0] -= fkx;
+                forces[k][1] -= fky;
+                forces[k][2] -= fkz;
+                forces[i][0] += fjx + fkx;
+                forces[i][1] += fjy + fky;
+                forces[i][2] += fjz + fkz;
 
-                    /* dV3/dR contributions to virial */
-                    virial -= s3[nj].r * (fjx * s3[nj].dx + fjy * s3[nj].dy + fjz * s3[nj].dz);
-                    virial -= s3[nk].r * (fkx * s3[nk].dx + fky * s3[nk].dy + fkz * s3[nk].dz);
+                /* dV3/dR contributions to virial */
+                virial -= s3[nj].r * (fjx * s3[nj].dx + fjy * s3[nj].dy + fjz * s3[nj].dz);
+                virial -= s3[nk].r * (fkx * s3[nk].dx + fky * s3[nk].dy + fkz * s3[nk].dz);
 
-                    /* prefactor for 4-body forces from coordination */
-                    if V3Z_on {
-                        let dxdZ = dwinv * (lcos + tau) + winv * dtau;
-                        let dV3dZ = temp1 * dHdx * dxdZ;
-                        /*--- LEVEL 4: LOOP FOR THREE-BODY COORDINATION FORCES ---*/
-                        for nl in 0..nz {
-                            sz[nl].sum += dV3dZ;
-                        }
-                    }
+                /* prefactor for 4-body forces from coordination */
+                let dxdZ = dw_inv * (lcos + tau) + w_inv * dtau;
+                let dV3dZ = temp1 * dHdx * dxdZ;
+                /*--- LEVEL 4: LOOP FOR THREE-BODY COORDINATION FORCES ---*/
+                for nl in 0..nz {
+                    sz[nl].sum += dV3dZ;
                 }
             }
-        } // end if V3_on
+        }
 
         /*--- LEVEL 2: LOOP TO APPLY COORDINATION FORCES ---*/
         for nl in 0..nz {
